47331f12dd26c6bd9bc19b3b943741c3ff310f15
Michael <mjg.py3@gmail.com>
2015-04-22 21:43:37 -0400

    Basic aliasing and example

--- README.md
+++ README.md
@@ -7,3 +7,31 @@ Ensure you have `python` installed, and run the install script
 ```
 . install.sh
 ```
+
+## Configuring (by example)
+
+If you wanted the command
+```
+git log
+```
+
+to be aliased to
+```
+afsd g l
+```
+
+you could do so with
+```
+[
+  {
+    "token": "g",
+    "command": "git",
+    "branches": [
+      {
+        "token": "l",
+        "command": "log"
+      }
+    ]
+  }
+]
+```
--- src/aka.py
+++ src/aka.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python
 
+import json
 import os
+import sys
 
-print(os.environ['AKA_ALIASES'])
+def raw_aliases():
+    '''
+    Reads in the aliases file as a Python object
+    '''
+    with open(os.environ['AKA_ALIASES'], 'r') as f:
+        return json.loads(f.read())
+
+def make_lookup(alias_object, current_path=[], current_command=[], result={}):
+    '''
+    Given a raw alias object, make it easily searchable
+    '''
+
+    for alias in alias_object:
+        p = current_path + [alias['token']]
+        c = current_command + [alias['command']]
+        result[tuple(p)] = ' '.join(c)
+
+        make_lookup(alias.get('branches', []), p, c, result)
+
+    return result
+
+
+lookup = make_lookup(raw_aliases())
+
+print(lookup)
+print(sys.argv)[1:]
+
+os.system(lookup[tuple(sys.argv[1:])])
